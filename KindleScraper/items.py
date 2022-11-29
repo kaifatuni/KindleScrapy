6c744d75bc28224e8615836d5a05f73843235af1
Mohammad Kaif <95179984+kaifatuni@users.noreply.github.com>
2022-11-29 23:18:00 +0530

    ItemLoader

--- KindleScraper/items.py
+++ KindleScraper/items.py
@@ -5,6 +5,69 @@
 
 import scrapy
 
+from itemloaders.processors import (MapCompose,
+                                    Join)
+
+from w3lib.html import remove_tags
+
+def col_whitespace(data):
+    # print('\n\ndata:', data)
+    # if data != None:
+    return data.split('\n')[0].strip()
+
+def first_num_from_str(data):
+    return [int(i) for i in data.replace('#', ' ').split() if i.isdigit()][0]
+
+def nums_from_str_hash(data):
+    return [int(i) for i in data.replace('#', ' ').split() if i.isdigit()]
+
+def nums_from_str_percent(data):
+    return [int(i) for i in data.replace('%', ' ').split() if i.isdigit()]
+
+def first_num_from_str_coma(data):
+    return [int(i) for i in data.replace(',', '').split() if i.isdigit()][0]
+
+def price(data):
+    # print('\n\n',data)
+    data1 = data.split('\n')[0].strip()
+    data2 = float(data.split('₹')[1].split()[0])
+    # data3 = [int(i) for i in data2.split() if i.isdigit()][0]
+    return { data1:data2 }
+    
+    
+class KindleProductDetails(scrapy.Item):
+    
+    
+    product_value = scrapy.Field()
+    
+    product_type=scrapy.Field(
+        input_processor = MapCompose(remove_tags, str.strip, price)
+    )
+    
+    product_col = scrapy.Field(
+        input_processor = MapCompose(col_whitespace),
+    )
+    best_seller=scrapy.Field(
+        input_processor = Join(),
+        output_processor = MapCompose(nums_from_str_hash)
+        
+    )
+    best_seller_rank=scrapy.Field(
+        input_processor = Join(),
+        output_processor = MapCompose(first_num_from_str)
+    )
+    other_cat=scrapy.Field()
+    
+    total_reviews = scrapy.Field(
+        input_processor = Join(),
+        output_processor = MapCompose(first_num_from_str_coma)
+    )
+    reviews_percent = scrapy.Field(
+        input_processor = Join(),
+        output_processor = MapCompose(nums_from_str_percent)
+    )
+
+
 class KindleProductItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
---
+++ KindleScraper/spiders/kind_pro_itemloa.py
@@ -0,0 +1,47 @@
+import scrapy
+from ..items import KindleProductDetails
+
+from itemloaders import ItemLoader
+
+class KindleBotSpider(scrapy.Spider):
+    name = 'kindle-product-itemloader'
+    count=1
+    start_urls = [
+        # 'file:D:\Codes\Python\KindleScraper\product.html'
+        'https://www.amazon.in/dp/B074LZG7KS'
+        # "https://www.amazon.com/s?k=iphone&page=1&qid=1624792241&ref=sr_pg_2"               
+        ]
+
+    def parse(self, response):
+        
+        
+        loader = ItemLoader(item=KindleProductDetails(), selector=response)
+        
+        
+        
+        
+        # value=response.css("#detailBullets_feature_div .a-text-bold+ span::text").extract()
+        # col=response.css("#detailBulletsWrapper_feature_div #detailBullets_feature_div .a-text-bold::text").extract()
+        
+        loader.add_css('product_value' , '#detailBullets_feature_div .a-text-bold+ span::text')
+        loader.add_css('product_col' , '#detailBulletsWrapper_feature_div #detailBullets_feature_div .a-text-bold::text')
+        loader.add_css('best_seller' , '#detailBullets_feature_div+ .detail-bullet-list > li > .a-list-item span::text')
+        loader.add_css('other_cat' , '.zg_hrsr a::text')
+        loader.add_css('best_seller_rank', '#detailBullets_feature_div+ .detail-bullet-list > li > .a-list-item::text')
+
+        
+        loader.add_css('total_reviews' , '.averageStarRatingNumerical .a-color-secondary::text')
+        loader.add_css('reviews_percent' , '.a-nowrap .a-link-normal::text')
+        
+        loader.add_css('product_type' , '#tmmSwatches .a-button-text')
+        
+        
+        item = loader.load_item()
+        yield item 
+        
+        # product=KindleProductItem()
+        # product["product_value"]=value
+        # product["product_col"]=col
+        # product["best_seller"]=best_seller
+        # product["other_cat"]=other_cat
+        # yield product
